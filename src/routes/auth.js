4163f6512183c3ae93433ade9acda79ce3a7aff9
Aleksey <email>
2020-08-23 15:32:03 +0300

    Refactor login router

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,15 +5,25 @@ import { sendResetPasswordEmail } from "../mailer";
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", async function (req, res) {
   const { credentials } = req.body;
-  User.findOne({ email: credentials.email }).then((user) => {
-    if (user && user.isValidPassword(credentials.password)) {
-      res.json({ user: user.toAuthJSON() });
+
+  try {
+    const user = await User.findOne({ email: credentials.email });
+    if (user) {
+      (await user.isValidPassword(credentials.password))
+        ? await res.json({ user: user.toAuthJSON() })
+        : await res
+            .status(400)
+            .json({ error: { password: "Not correct password" } });
     } else {
-      res.status(400).json({ errors: { global: "Invalid credentials " } });
+      await res
+        .status(400)
+        .json({ error: { email: "No user with this email" } });
     }
-  });
+  } catch (e) {
+    await res.status(500).json({ error: { global: "Error. Something went wrong..." } } );
+  }
 });
 
 router.post("/confirmation", (req, res) => {
@@ -52,14 +62,14 @@ router.post("/reset_password_request", async function (req, res) {
 
 router.post("/validate_token", (req, res) => {
   const { token } = req.body;
-  jwt.verify(token, process.env.JWT_SECRET, err => {
+  jwt.verify(token, process.env.JWT_SECRET, (err) => {
     if (err) {
-      res.status(401).json({})
+      res.status(401).json({});
     } else {
-      res.json({})
+      res.json({});
     }
-  })
-})
+  });
+});
 
 router.post("/reset_password", (req, res) => {
   const { password, token } = req.body.data;
@@ -67,11 +77,10 @@ router.post("/reset_password", (req, res) => {
     if (err) {
       res.status(401).json({ errors: { global: "Invalid token" } });
     } else {
-      User.findOne({ _id: decoded._id }).then(user => {
+      User.findOne({ _id: decoded._id }).then((user) => {
         if (user) {
           user.setPassword(password);
-          user.save()
-            .then(() => res.json({}));
+          user.save().then(() => res.json({}));
         } else {
           res.status(404).json({ errors: { global: "Invalid token" } });
         }
